42238684f0d58a920e57b3c93c25dbc89502d296
voidisnotart <160656575+voidisnotart@users.noreply.github.com>
2024-02-20 21:08:41 +0000

    ok

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ const util = require('util');
 module.exports = function(eleventyConfig) {    
     eleventyConfig.addPassthroughCopy("**/*.jpg");
     eleventyConfig.addPassthroughCopy("**/*.png");
+    eleventyConfig.addPassthroughCopy("./styles.css");
+      
+
     eleventyConfig.addFilter('console', function(value) {
         const str = util.inspect(value);
         return `<div style="white-space: pre-wrap;">${unescape(str)}</div>;`
