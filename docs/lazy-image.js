90e77db7f30dbd8e56d729a4c9146e1cebbbf813
voidisnotart <voidisnotart@users.noreply.github.com>
2024-02-21 10:26:42 +0000

    Apply automatic changes

--- docs/lazy-image.js
+++ docs/lazy-image.js
@@ -1,4 +1,19 @@
-var images = document.querySelectorAll('img[loading="lazy"]')
+var images = document.querySelectorAll('img[loading="lazy]')
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(function (entry) {
+//         if (!entry.isIntersecting) return 
+
+//         entry.target.src = entry.target.getAttribute('data-src')
+//         observer.unobserve(image)
+//     })
+// }, {
+//     rootMargin: '0px 0px 512px 0px',
+// })
+
+// images.forEach(function (image) {
+//     observer.observe(image)
+// })
 
 images.forEach(function (image) {
     image.addEventListener('load', function onLoad() {
